06a14ff8bd590de93527298463951588d8dcb87e
scgisme <32117203+scgisme@users.noreply.github.com>
2017-09-19 21:41:11 -0500

    Initial Shared Code
    have at it.

---
+++ twentyone.py
@@ -0,0 +1,39 @@
+print("Welcome to Scooter's Casino BlackJack Table \n")
+playername = input("What is your name sucker.. eh .. I mean player? ")
+print('Fantastic to have you %s !!!'%(playername))
+playerbalance=input('How much money do you have to chip up?')
+
+while 'True':
+    
+    class Dealer(object):
+        def __init__(self):
+            self.deck=set()
+            self.hand=set()
+            
+        def hitplayer():
+            pass
+        
+        def deal():
+            pass
+                 
+    class Player(object):
+        def __init__(self,chips,name):
+            self.chips = chips
+            self.name = name
+            self.bet = bet
+            self.hand = set()
+        
+        def placebet():
+            pass
+        
+        def hold():
+            pass
+        
+    
+    """
+    Below is where the game begins
+    """
+    
+    print('you currently have %s'%(playerbalance))
+    playerwager=input('Ok, so how much do you want to wager on this hand of blackjack?  Me vs You punk!')
+    break
